94e8fa808715fccd50b7efa1da0ea4f8603fa96a
newtonch66 <chakman828@gmail.com>
2026-02-20 23:11:56 +0600

    CashOut js done

--- script/cashout.js
+++ script/cashout.js
@@ -2,10 +2,34 @@ document.getElementById('cashout-btn').addEventListener('click', function(){
     const cashoutNumberInput = document.getElementById('cashout-number');
     const cashoutNumber = cashoutNumberInput.value;
     console.log(cashoutNumber);
+    if(cashoutNumber.length !=11){
+        alert("Invalid Agent Number");
+        return;
+    }
     const cashoutAmountInput = document.getElementById('cashout-amount');
     const cashoutAmount = cashoutAmountInput.value;
     console.log(cashoutAmount);
+
     const balanceElement = document.getElementById('balance');
     const balance = balanceElement.innerText;
     console.log(balance);
+
+    const newBalance= Number(balance) - Number(cashoutAmount);
+    if( newBalance < 0){
+        alert('Invalid Amount');
+        return;
+    }
+    console.log(newBalance)
+    const cashoutPinInput = document.getElementById('cashout-pin')
+    const pin = cashoutPinInput.value;
+    if( pin==="1234"){
+        alert("CashOut successfull");
+        console.log(newBalance);
+        balanceElement.innerText=newBalance
+
+    }
+    else{
+        alert("Invalid Pin");
+        return;
+    }
 })
